7b9967aefec83982847f9ebb217f8a90d127fb1b
Savero Arkabuana <savero.arkabauana@gmail.com>
2022-09-11 23:41:09 +0700

    update

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+
+def show_katalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+        "list_barang" : data_barang_catalog,
+        "nama" : "Savero Arkabuana",
+        "id" : "2106635985",
+    }
+    return render(request, "katalog.html", context)
