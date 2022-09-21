f658f076fa3824e412e59a5f8033cca3d969a65c
Savero Arkabuana <savero.arkabauana@gmail.com>
2022-09-22 05:08:57 +0700

    tugas3

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -9,6 +9,18 @@ class mywatchlist(TestCase):
         response = Client().get('/mywatchlist/')
         self.assertEqual(response.status_code,200)
 
+    def test_mywatchlisthtml_exists(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+
+    def test_mywatchlistjson_exists(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+
+    def test_mywatchlistxml_exists(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+
     def test_contoh_app_using_to_do_list_template(self):
         response = Client().get('/mywatchlist/')
         self.assertTemplateUsed(response, "mywatchlist.html")
--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -8,6 +8,7 @@ app_name = 'mywatchlist'
 
 urlpatterns = [
     path('', show_mywatchlist, name='show_mywatchlist'),
+    path('html/', show_mywatchlist, name='show_mywatchlist'),
     path('xml/', show_xml, name="show_xml"),
     path('json/', show_json, name="show_json"),
     path('json/<int:id>', show_json_by_id, name="show_json_by_id"),
