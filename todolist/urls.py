36f986c9ed6c5421adb694711eefffbd0a3047fb
Savero Arkabuana <savero.arkabauana@gmail.com>
2022-09-28 13:19:24 +0700

    tugas 4 collectstatic

--- todolist/urls.py
+++ todolist/urls.py
@@ -9,9 +9,10 @@ from todolist.views import create_task
 app_name = 'todolist'
 
 urlpatterns = [
-    path('', show_todolist, name='show_todolist'),
+    path('', login_user, name='login'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('create-task/', create_task, name='createtask'),
+
 ]
